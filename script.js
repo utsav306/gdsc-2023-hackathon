8f33749537ba727dbffa0b20817f2181b4b015e1
Utsav <utsavtiwari030@outlook.com>
2023-03-05 08:06:58 +0530

    NavbarUpdate

---
+++ script.js
@@ -0,0 +1,8 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('sign-in');
+
+
+signInButton.addEventListener('click', () => {
+	log.style.display="block";
+});
+
