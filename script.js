04f16aeb1afb4460dc3351bf6d37d6405cbd50d6
Utsav <utsavtiwari030@outlook.com>
2023-03-06 20:21:26 +0530

    bug fix

--- script.js
+++
@@ -1,8 +0,0 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('sign-in');
-
-
-signInButton.addEventListener('click', () => {
-	log.style.display="block";
-});
-
